9b5a1aff80b2289e8233d9b316b4a02498a0d8bc
Shamir C <scharania@gmail.com>
2013-03-31 23:00:17 -0600

    Added RuleAnalyzer.  Changed some printing options and updated the readme

---
+++ RuleAnalyzer.py
@@ -0,0 +1,76 @@
+#! /usr/bin/env python
+
+##########################################
+# Log Parser Main
+#
+#
+# Written By Shamir Charania
+##########################################
+
+import argparse
+import CheckpointLog
+import Helper
+
+parser = argparse.ArgumentParser()
+parser.add_argument("file",type=str,help="The file to parse")
+parser.add_argument("rule",type=str,help="    The rule to search.  This actually searches the \
+                                              current rule number column so probably like 555-Policy-Name")
+
+args = parser.parse_args()
+
+checkpointLog = CheckpointLog.CheckpointLog(args.file)
+
+mustHaveHeaders = [
+                   "Origin",
+                   "Source",
+                   "Destination",
+                   "Service",
+                   "Protocol",
+                   "Current Rule Number"
+                   ]
+
+checkpointLog.HasHeaders(mustHaveHeaders)
+
+print "Parsing {0}, looking for rule {1}".format(args.file,args.rule)
+
+sources = {}
+destinations = {}
+origins = {}
+protocols = {}
+flows = {}
+
+matchingLines = 0
+
+values = checkpointLog.GetLineByHeader(mustHaveHeaders)
+while (len(values)):
+    origin = values[0]
+    source = values[1]
+    destination = values[2]
+    service = values[3]
+    protocol = values[4]
+    rule = values[5]
+    
+    if not (rule == args.rule): 
+        values = checkpointLog.GetLineByHeader(mustHaveHeaders)
+        continue
+    
+    Helper.AddToDict(source,sources)
+    Helper.AddToDict(destination,destinations)
+    Helper.AddToDict("Protocol " + protocol.ljust(5) + " Service: " + service.ljust(20) + " {0} ==> {1}".format(source,destination) ,flows)
+    Helper.AddToDict(origin, origins)
+    Helper.AddToDict(protocol,protocols)
+    matchingLines += 1
+    values = checkpointLog.GetLineByHeader(mustHaveHeaders)
+
+checkpointLog.Close()
+
+print "Total lines read {0}".format(checkpointLog.GetLinesRead())
+print "Total matching lines {0}".format(matchingLines)
+print ""
+
+
+Helper.PrintTop10(sources,"Sources")
+Helper.PrintTop10(destinations,"Destinations")
+Helper.PrintTop10(flows,"Flows")
+Helper.PrintTop10(origins, "Origins")
+Helper.PrintTop10(protocols,"Protocols")
